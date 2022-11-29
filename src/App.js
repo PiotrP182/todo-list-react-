dcd35805a226bc2c9d503c2792a29f8b5289366a
piotrp182 <przybylpio@gmail.com>
2022-11-29 18:06:36 +0100

    todo list with section, header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
+import Section from "./Section";
+import Header from "./Header";
+import Container from "./Container";
 
 const tasks = [
   {id: 1, content: "przejść na Reacta", done: false},
@@ -11,30 +14,18 @@ const hideDoneTasks = false;
 
 function App() {
   return (
-    <main className="container">
-      <header>
-        <h1>Lista zadań</h1>
-      </header>
-      <section className="section">
-        <header className="section__header">
-          <h2 className="section__title">Dodaj nowe zadanie</h2>
-        </header>
-
-        <div className="section__body">
-          <Form />
-        </div>
-      </section>
-
-      <section className="section">
-        <header className="section__header">
-          <h2 className="section__title">Lista zadań</h2>
-          <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
-        </header>
-        <div className="section__body">
-          <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
-          </div>
-      </section>
-    </main>
+    <Container className="container">
+      <Header title="Lista zadań" />
+      <Section 
+      title="Dodaj nowe zadanie" 
+      body={<Form />} 
+      />
+      
+      <Section title="Lista zadań"
+      body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+      extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+      />
+    </Container>
   );
 }
 
